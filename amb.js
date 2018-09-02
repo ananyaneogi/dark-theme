4f81f75b42cb6da7ed1bfe03905399851914e76b
ananyaneogi <ananyaneogi11@gmail.com>
2018-09-02 20:22:18 +0530

    initial commit

---
+++ amb.js
@@ -0,0 +1,19 @@
+console.log('starting...');
+if ( 'AmbientLightSensor' in window ) {
+  const sensor = new AmbientLightSensor();
+  sensor.onreading = () => {
+    console.log('illuminance level :', sensor.illuminance);
+      if(sensor.illuminance < 15) {
+            document.querySelector('body').classList.add("black");
+            document.querySelector('body').classList.remove("white");
+      }
+      else {
+            document.querySelector('body').classList.add("white");
+            document.querySelector('body').classList.remove("black");
+      }
+  };
+  sensor.onerror = (event) => {
+    console.log(event.error.name, event.error.message);
+  };
+  sensor.start();
+}
